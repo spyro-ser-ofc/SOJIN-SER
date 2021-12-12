41c85c087102113ccc2f071c8c7e433d0fb95b7b
Aju <94348613+aju001@users.noreply.github.com>
2021-12-12 17:26:44 +0530

    AJUX

--- Bosco.js
+++ Bosco.js
@@ -1408,7 +1408,7 @@ break
 					teks = (args.length > 1) ? body.slice(8).trim() : ''
 					teks += '\n\n'
 					for (let mem of groupMembers) {
-						teks += `👀 @${mem.jid.split('@')[0]}\n🫂`
+						teks += `🫂 @${mem.jid.split('@')[0]}👤\n`
 						members_id.push(mem.jid)
 					}
 					mentions(teks, members_id, true)
